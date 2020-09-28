e97b08815b1acbdec3c0e7567edac8588ff165df
QAQ-wangyizhang <780317879@qq.com>
2020-09-28 20:45:06 +0800

    2020-09-28

---
+++ react-learn/src/components/Pager.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// 
+
+export default function Pager(props) {
+
+
+    return (
+        <>
+            <span className="item">首页</span>
+        </>
+    )
+}
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StudentList from "./components/StudentList"
-
-const appkey = "QAQwangyizhang_1585626591787";
-async function fetchAllStudnets(){
-  const stus = await fetch("http://open.duyiedu.com/api/student/findAll?appkey=" + appkey)
-                        .then(res => res.json());
-  return stus;
-}
-
-async function render(){
-  ReactDOM.render((<h3>正在加载中...</h3>),document.getElementById("root"))
-  const s = await fetchAllStudnets();
-  // console.log(s.data);
-  ReactDOM.render((<div>
-    <StudentList stus={s.data}></StudentList>
-  </div>),document.getElementById("root"))
-}
-// let s = fetchAllStudnets().then(res => res);
-// console.log(s);
-render();
-
+import Pager from "./components/Pager"
+ReactDOM.render(<Pager current={1}
+                        total= {20}
+                        limit = {5}
+                        panelNumber = {10}
+></Pager>, document.getElementById("root"))
