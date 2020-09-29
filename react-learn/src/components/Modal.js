00afd3dcd90c6aa571498ab676de5cbba29bd9fc
QAQ-wangyizhang <780317879@qq.com>
2020-09-29 09:54:54 +0800

    2020-09-29

--- react-learn/src/components/Modal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./modal.css"
-export default function Modal(props) {
-    return (
-        <div className={props.show? "modal": "modal none"}>
-            <p>加载中...</p>
-        </div>
-    )
-}
--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,11 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Pager from "./components/Pager"
-import PagerTest from "./components/PagerTest"
-import "./index.css"
-// ReactDOM.render(<Pager current={2}
-//                         total= {100}
-//                         limit = {9}
-//                         panelNumber = {10}
-// ></Pager>, document.getElementById("root"))
+
 ReactDOM.render(<PagerTest></PagerTest>, document.getElementById("root"))
