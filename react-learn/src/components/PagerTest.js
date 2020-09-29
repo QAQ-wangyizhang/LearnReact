74cf8462d99f8e5ad574cfdf0aea35de9323c63d
QAQ-wangyizhang <780317879@qq.com>
2020-09-29 09:52:10 +0800

    2020-09-29

--- react-learn/src/components/PagerTest.js
+++ react-learn/src/components/PagerTest.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react'
+import Modal from './Modal';
 import Pager from "./Pager"
 import StudnetList from "./StudentList"
-const appkey = 'qfjdskjfsffd_1599095975670';
-async function getData(page,size) {
-    let res = await fetch(`http://open.duyiedu.com/api/student/findByPage?appkey=${appkey}&page=${page}&size=${size}`)
-                    .then(res => res.json());
-    return res;
-}
+
+
 export default class PagerTest extends Component {
     constructor(props) {
         super(props)
+        this.appkey = 'qfjdskjfsffd_1599095975670';
         this.state = {
             current: 2, // 当前页
             total: 100, // 数据的条数
-            limit: 2, // 一页最多展示多少条
+            limit: 1, // 一页最多展示多少条
             panelNumber: 3, // 展示页的条数
-            stus : []
+            stus : [],
+            isLoading : true
         }
         this.renderData(this.state.current,this.state.limit);
     }
 
     async renderData(page,size){
-        let res = await getData(page,size);
+        this.setState({
+            isLoading : true
+        })
+        let res = await this.getData(page,size);
         this.setState({
             total : res.data.cont,
             stus : res.data.findByPage,
             limit : size,
-            current : page
+            current : page,
+            isLoading : false
         })
+    }
+
+    async getData(page,size) {
+        let res = await fetch(`http://open.duyiedu.com/api/student/findByPage?appkey=${this.appkey}&page=${page}&size=${size}`)
+                        .then(res => res.json());
+        return res;
     }
 
     onPageChange = (target) => {
@@ -35,13 +44,13 @@ export default class PagerTest extends Component {
             current: target
         })
         this.renderData(target,this.state.limit)
-        // console.log("1")
     }
     render() {
         return (
             < >
                 <StudnetList stus={this.state.stus}></StudnetList>
                 <Pager {...this.state} onPageChange={this.onPageChange}></Pager>
+                <Modal show={this.state.isLoading}></Modal>
             </>
         )
     }
