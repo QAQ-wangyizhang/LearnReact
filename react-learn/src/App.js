5300ec840d76a614fec68641d7ab3b05b470422e
QAQ-wangyizhang <780317879@qq.com>
2020-10-04 10:04:42 +0800

    2020-10-04

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,35 +1,12 @@
-import React, { Component } from 'react'
-// import OldLifeCycle from "./OldLifeCycle"
-import NewLifeCycle from "./NewLifeCycle"
+import React from 'react'
 
-export default class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            n : 0,
-            show : true
-        }
-    }
-    handleClick = _ => {
-        this.setState({
-            n : this.state.n + 1
-        })
-    }
-    
-    render() {
-        const comp = this.state.show ? <NewLifeCycle n={this.state.n}></NewLifeCycle> : null
-        return (
-            <div>
-                {comp}
-                <button onClick={this.handleClick}>属性n+1</button>
-                <button onClick={()=>{
-                    this.setState({
-                        show : !this.state.show
-                    })
-                }}>显示/隐藏</button>
-            </div>
-        )
-    }
+export default function App(props) {
+    return (
+        <div className="app">
+            {props.html}
+            {props.children}
+            {props.div || "没有啊"}
+        </div>
+    )
 }
-
 
